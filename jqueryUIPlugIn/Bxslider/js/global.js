26a197d8515f970a5f1ef2f9686e04380c460466
OSG <notdonkatsu@gmail.com>
2021-01-24 21:43:34 +0900

    bxSlider Basic

---
+++ jqueryUIPlugIn/Bxslider/js/global.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#slider').bxSlider({
+        'mode' : 'horizontal',
+        'slideSelector' : 0,
+        'randomStart' : true,
+        'pager' : false,
+        'auto' : true,
+        'autoHover' : true,
+        'controls' : true,
+        'touchEnabled' : false,
+        'easing' : 'ease-out',
+        'slideWidth' : '1000px'
+    });
+});
